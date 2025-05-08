b1f2ec8b488441929244f8125480fd1d22a585e7
cpaluszek <paluszek.clement@proton.me>
2025-05-08 20:15:59 +0200

    update syntax for vim opt

--- lua/cpaluszek/vim.lua
+++ lua/cpaluszek/vim.lua
@@ -1,15 +1,15 @@
 local function set_vim_opt()
-  vim.opt.number = true,
-  vim.opt.relative_number = true,
-  vim.opt.scrolloff = 4,
-  vim.opt.tabstop = 4,
-  vim.opt.shiftwidth = 4,
-  vim.opt.expandtab = true,
-  vim.opt.list = true,
-  vim.opt.wrap = false,
-  vim.opt.swapfile = false,
-  vim.opt.backup = false,
-  vim.opt.termguicolors = true,
+  vim.opt.number = true
+  vim.opt.relative_number = true
+  vim.opt.scrolloff = 4
+  vim.opt.tabstop = 4
+  vim.opt.shiftwidth = 4
+  vim.opt.expandtab = true
+  vim.opt.list = true
+  vim.opt.wrap = false
+  vim.opt.swapfile = false
+  vim.opt.backup = false
+  vim.opt.termguicolors = true
 end
 
 local function set_vim_keys()
