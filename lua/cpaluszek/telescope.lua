21b657a6295b45f00bfcabfd579685a50fc8cda7
cpaluszek <paluszek.clement@proton.me>
2025-05-08 21:53:13 +0200

    typo

--- lua/cpaluszek/telescope.lua
+++ lua/cpaluszek/telescope.lua
@@ -13,8 +13,8 @@ local function init()
 
   -- Builtin
   vim.keymap.set('n', '<leader>ff', '<CMD>lua require("telescope.builtin").find_files{ hidden = true }<CR>')
-  vim.keymap.set('n', '<leader>fs', '<CMD>lua require("telescope.builtin").grep_string<CR>')
-  vim.keymap.set('n', '<leader>fl', '<CMD>lua require("telescope.builtin").live_grep<CR>')
+  vim.keymap.set('n', '<leader>fs', '<CMD>lua require("telescope.builtin").grep_string()<CR>')
+  vim.keymap.set('n', '<leader>fl', '<CMD>lua require("telescope.builtin").live_grep()<CR>')
 end
 
 return {
