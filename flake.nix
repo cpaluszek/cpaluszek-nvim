ea1a8e44599e4e01ce97166be9e2e1044d6f56ee
cpaluszek <paluszek.clement@proton.me>
2025-05-08 17:16:26 +0200

    add justfile

--- flake.nix
+++ flake.nix
@@ -11,15 +11,21 @@
     in {
       lib = import ./lib {inputs = self.inputs; };
 
-      packages = {
+      packages.${system} = {
         default = self.lib.mkVimPlugin { inherit system; };
         neovim = self.lib.mkNeovim { inherit system; };
       };
 
-      apps = {
+      apps.${system} = {
         nvim = {
           program = "${self.packages.${system}.neovim}/bin/nvim";
           type = "app";
+        };
+      };
+
+      devShells.${system} = {
+        default = nixpkgs.legacyPackages.${system}.mkShell {
+          buildInputs = [nixpkgs.legacyPackages.${system}.just];
         };
       };
   };
