2b74910bba8b817760414fa473b9e65d4b8cc95c
cpaluszek <paluszek.clement@proton.me>
2025-05-08 20:11:26 +0200

    add base keymaps

--- lua/cpaluszek/vim.lua
+++ lua/cpaluszek/vim.lua
@@ -23,9 +23,30 @@ local function set_vim_opt()
   end
 end
 
+local function set_vim_keys()
+  vim.keymap.set('n', '<C-h>', '<CMD>wincmd h<CR>', {desc = 'Wincmd left', silent = true})
+  vim.keymap.set('n', '<C-j>', '<CMD>wincmd j<CR>', {desc = 'Wincmd down', silent = true})
+  vim.keymap.set('n', '<C-k>', '<CMD>wincmd k<CR>', {desc = 'Wincmd up', silent = true})
+  vim.keymap.set('n', '<C-l>', '<CMD>wincmd l<CR>', {desc = 'Wincmd right', silent = true})
+
+  vim.keymap.set('x', '<leader>p', '\"_dP', {desc = 'Paste selection without adding to buffer', silent = true})
+
+  -- copy to sys clipboard
+  vim.keymap.set('n', '<leader>y', '\"+y', {desc = 'Copy to sys clipboard'})
+  vim.keymap.set('v', '<leader>y', '\"+y', {desc = 'Copy to sys clipboard'})
+  vim.keymap.set('n', '<leader>Y', '\"+Y', {desc = 'Copy to sys clipboard'})
+
+  vim.keymap.set('n', '<leader>d', '\"_d', {desc = 'Delete and copy to sys clipboard'})
+  vim.keymap.set('v', '<leader>d', '\"_d', {desc = 'Delete and copy to sys clipboard'})
+
+  -- search and replace
+  vim.keymap.set('n', '<leader>s', ':%s/\\<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left>', {descr = 'Search and replace', silent = true})
+end
+
 local function init()
   vim.g.mapleader = " "
   set_vim_opt()
+  set_vim_keys()
 end
 
 return {
