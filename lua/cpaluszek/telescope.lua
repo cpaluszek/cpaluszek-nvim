cdadb4c937748ecf3a38ad0039a91432e9a776ac
cpaluszek <paluszek.clement@proton.me>
2025-05-08 22:02:31 +0200

    add telescope binds

--- lua/cpaluszek/telescope.lua
+++ lua/cpaluszek/telescope.lua
@@ -11,10 +11,20 @@ local function init()
     }
   }
 
+  local options = { noremap = true, silent = true }
   -- Builtin
-  vim.keymap.set('n', '<leader>ff', '<CMD>lua require("telescope.builtin").find_files{ hidden = true }<CR>')
-  vim.keymap.set('n', '<leader>fs', '<CMD>lua require("telescope.builtin").grep_string()<CR>')
-  vim.keymap.set('n', '<leader>fl', '<CMD>lua require("telescope.builtin").live_grep()<CR>')
+  vim.keymap.set('n', '<leader>ff', '<CMD>lua require("telescope.builtin").find_files{ hidden = true }<CR>', options)
+  vim.keymap.set('n', '<leader>fs', '<CMD>lua require("telescope.builtin").live_grep()<CR>', options)
+  vim.keymap.set('n', '<leader>fb', '<CMD>lua require("telescope.builtin").buffers()<CR>', options)
+  vim.keymap.set('n', '<leader>fh', '<CMD>lua require("telescope.builtin").help_tags()<CR>', options)
+  vim.keymap.set('n', '<leader>fd', '<CMD>lua require("telescope.builtin").diagnostics()<CR>', options)
+
+  -- Language servers
+  vim.keymap.set('n', '<leader>lsd', '<CMD>lua require("telescope.builtin").lsp_definitions{}<CR>', options)
+  vim.keymap.set('n', '<leader>lsi', '<CMD>lua require("telescope.builtin").lsp_implementations{}<CR>', options)
+  vim.keymap.set('n', '<leader>lsl', '<CMD>lua require("telescope.builtin").lsp_code_actions{}<CR>', options)
+  vim.keymap.set('n', '<leader>lst', '<CMD>lua require("telescope.builtin").lsp_type_definitions{}<CR>', options)
+  -- TODO: notify telescope extension
 end
 
 return {
